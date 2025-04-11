74f9530b4916e82b62271d5e5ef522131c696988
Damir Karimov <dkarimov@alfabank.ru>
2025-04-11 15:55:09 +0300

    still broken await

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
-use web_sys::window;
 use eframe::egui;
 
+#[cfg(target_arch = "wasm32")]
+use web_sys::window;
+
+#[cfg(target_arch = "wasm32")]
+use eframe::wasm_bindgen::JsCast;
+
+#[cfg(not(target_arch = "wasm32"))]
+fn main() -> eframe::Result<()> {
+    eframe::run_native(
+        "calculator",
+        eframe::NativeOptions::default(),
+        Box::new(|creation_context| Ok(Box::new(App::new(creation_context)))),
+    )
+}
+
+#[cfg(target_arch = "wasm32")]
 fn main() {
     console_error_panic_hook::set_once();
 
@@ -11,4 +26,43 @@ fn main() {
     let text_node = document.create_text_node("Hello, world from Vanilla Rust!");
     body.append_child(text_node.as_ref())
         .expect("Failed to append text");
+
+    let canvas = document
+        .create_element("canvas")
+        .expect("Failed to create canvas");
+    body.append_child(canvas.as_ref())
+        .expect("Failed to append canvas");
+    let canvas: web_sys::HtmlCanvasElement = canvas
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .map_err(|_| ())
+        .unwrap();
+
+    let runner = eframe::WebRunner::new();
+
+    runner.start(
+        canvas,
+        eframe::WebOptions::default(),
+        Box::new(|cc| Ok(Box::new(App::new(cc)))),
+    );
+}
+
+struct App {}
+
+impl App {
+    fn new(_creation_context: &eframe::CreationContext) -> Self {
+        App {}
+    }
+}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hello World!");
+            ui.label(r#"  
+            Это вымышленный калькулятов.  
+            Чтобы воспользоваться калькулятором вам необходимо воспользоваться воображением.            Представте себе любой интерфейс, наберите в нем математическое выражение и нажмите '='.  
+            Увидели результат, Да? - Поздравляю, ваш калькулятор работает хорошо.  
+            "#);  
+        });
+    }
 }
