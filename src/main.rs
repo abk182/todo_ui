119407b9c650f2de8e4d8470733dfb252bb495ca
Damir Karimov <dkarimov@alfabank.ru>
2025-04-11 15:18:06 +0300

    reinit

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,14 @@
+use web_sys::window;
 use eframe::egui;
 
-#[cfg(not(target_arch = "wasm32"))]
-fn main() -> eframe::Result<()> {
-    eframe::run_native(
-        "calculator",
-        eframe::NativeOptions::default(),
-        Box::new(|creation_context| Ok(Box::new(App::new(creation_context)))),
-    )
-}
-
-#[cfg(target_arch = "wasm32")]
 fn main() {
-    use eframe::wasm_bindgen::JsCast as _;
+    console_error_panic_hook::set_once();
 
-    wasm_bindgen_futures::spawn_local(async {
-        let document = web_sys::window()
-            .expect("No window")
-            .document()
-            .expect("No document");
-
-        let canvas = document
-            .get_element_by_id("calculator")
-            .expect("Failed to find the_canvas_id")
-            .dyn_into::<web_sys::HtmlCanvasElement>()
-            .expect("the_canvas_id was not a HtmlCanvasElement");
-        
-        let start_result = eframe::WebRunner::new()
-            .start(
-                canvas,
-                eframe::WebOptions::default(),
-                Box::new(|cc| Ok(Box::new(App::new(cc)))),
-            )
-            .await;
-    })
-}
-
-struct App {}
-
-impl App {
-    fn new(_creation_context: &eframe::CreationContext) -> Self {
-        App {}
-    }
-}
-
-impl eframe::App for App {
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Hello World!");
-            ui.label(r#"  
-            Это вымышленный калькулятов.  
-            Чтобы воспользоваться калькулятором вам необходимо воспользоваться воображением.            Представте себе любой интерфейс, наберите в нем математическое выражение и нажмите '='.  
-            Увидели результат, Да? - Поздравляю, ваш калькулятор работает хорошо.  
-            "#);  
-        });
-    }
+    let document = window()
+        .and_then(|win| win.document())
+        .expect("Could not access the document");
+    let body = document.body().expect("Could not access document.body");
+    let text_node = document.create_text_node("Hello, world from Vanilla Rust!");
+    body.append_child(text_node.as_ref())
+        .expect("Failed to append text");
 }
