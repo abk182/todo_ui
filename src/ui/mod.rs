51fd8bd75aaeb577124c85d8ec251a1ce86ee9aa
Damir Karimov <dkarimov@alfabank.ru>
2025-04-16 17:52:38 +0300

    add text edit

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,9 +1,17 @@
+mod todo_list;
+
 use eframe::egui;
-pub struct App {}
+use todo_list::TodoList;
+
+pub struct App {
+    todo_list: TodoList,
+}
 
 impl App {
     pub fn new(_creation_context: &eframe::CreationContext) -> Self {
-        App {}
+        App {
+            todo_list: TodoList::new(),
+        }
     }
 }
 
@@ -16,6 +24,8 @@ impl eframe::App for App {
             Чтобы воспользоваться калькулятором вам необходимо воспользоваться воображением.            Представте себе любой интерфейс, наберите в нем математическое выражение и нажмите '='.  
             Увидели результат, Да? - Поздравляю, ваш калькулятор работает хорошо.  
             "#);  
+
+            self.todo_list.show(ui);
         });
     }
 }
---
+++ src/ui/todo_list.rs
@@ -0,0 +1,31 @@
+use eframe::egui;
+
+pub struct TodoList {
+    list: Vec<String>,
+}
+
+impl TodoList {
+    pub fn new() -> Self {
+        TodoList {
+            list: vec![String::from("aaaa"), String::from("aaaa")],
+        }
+    }
+
+    pub fn show(&mut self, ui: &mut egui::Ui) {
+        for el in &self.list {
+            ui.add(egui::Label::new(el));
+        }
+
+        for el in &mut self.list {
+            // ui.add(egui::Label::new(el));
+
+            let t = egui::TextEdit::multiline(el)
+                .font(egui::TextStyle::Monospace) // for cursor height
+                .code_editor()
+                .desired_rows(10)
+                .lock_focus(true);
+
+            ui.add(t);
+        }
+    }
+}
