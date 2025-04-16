1dba07dc9d028426eaf1912a7774a7db09e2d503
Damir Karimov <dkarimov@alfabank.ru>
2025-04-16 15:44:56 +0300

    ready to dev ui

--- src/main.rs
+++ src/main.rs
@@ -23,17 +23,9 @@ fn main() {
     let document = window()
         .and_then(|win| win.document())
         .expect("Could not access the document");
-    let body = document.body().expect("Could not access document.body");
-    let text_node = document.create_text_node("Hello, world from Vanilla Rust!");
-    body.append_child(text_node.as_ref())
-        .expect("Failed to append text");
-
     let canvas = document
-        .create_element("canvas")
-        .expect("Failed to create canvas");
-    body.append_child(canvas.as_ref())
-        .expect("Failed to append canvas");
-    let canvas: web_sys::HtmlCanvasElement = canvas
+        .get_element_by_id("canvas-for-egui")
+        .expect("No element with id")
         .dyn_into::<web_sys::HtmlCanvasElement>()
         .map_err(|_| ())
         .unwrap();
