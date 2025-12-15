994e070e1e74272c252be4e0d5761c2babcebe9d
Damir Karimov <dkarimov@alfabank.ru>
2025-12-15 17:30:48 +0300

    apply crud

--- src/native/todo_list/todo/mod.rs
+++ src/native/todo_list/todo/mod.rs
@@ -1,45 +1,27 @@
 use crate::utils::DATE_FORMAT;
 use chrono;
-use eframe::egui;
 pub struct Todo {
-    date: chrono::DateTime<chrono::Local>,
-    content: String,
+    pub date: chrono::DateTime<chrono::Local>,
+    pub content: String,
+    pub file_path: String,
 }
 
 impl Todo {
-    pub fn new(date: Option<&str>, content: &str) -> Self {
+    pub fn new(date: Option<&str>, content: &str, file_path: &str) -> Self {
         if let Some(date) = date {
             Todo {
                 date: chrono::DateTime::parse_from_str(date, DATE_FORMAT)
                     .unwrap()
                     .into(),
                 content: String::from(content),
+                file_path: String::from(String::from(file_path)),
             }
         } else {
             Todo {
                 date: chrono::offset::Local::now(),
                 content: String::from(content),
+                file_path: String::from(String::from(file_path)),
             }
         }
-    }
-
-    pub fn show(&mut self, ui: &mut egui::Ui) {
-        ui.vertical(|ui| {
-            let date_str = format!("Date: \n{}", self.date.format(DATE_FORMAT));
-
-            let label = egui::Label::new(date_str);
-
-            let text_edit = egui::TextEdit::multiline(&mut self.content)
-                .font(egui::TextStyle::Monospace) // for cursor height
-                .code_editor()
-                .desired_rows(10)
-                .lock_focus(true);
-
-            let button = egui::Button::new("Save");
-
-            ui.add(label);
-            ui.add(text_edit);
-            ui.add(button);
-        });
     }
 }
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,1 +1,3 @@
 pub const DATE_FORMAT: &str = "%Y-%m-%d %H:%M:%S %z";
+pub const TODOS: &str = "todos";
+pub const FORMAT: &str = "txt";
